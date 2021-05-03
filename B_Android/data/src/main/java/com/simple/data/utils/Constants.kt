f20867cccbab6ba9b497dc6c979256421937b201
DGSW_양동현 <66651059+DGSWDongHyun@users.noreply.github.com>
2021-05-03 20:33:20 +0900

    bindingadapter 수정

--- B_Android/data/src/main/java/com/simple/data/utils/Constants.kt
+++ B_Android/data/src/main/java/com/simple/data/utils/Constants.kt
@@ -8,8 +8,9 @@ class Constants {
         const val NEIS_API_KEY = "503bcc6acfd4461f8fdf117c90319b51"
         const val DEFAULT_TYPE = "json"
         const val SIZE = 21
-        val DEFAULT_DATE = Calendar.getInstance()
+        val DEFAULT_DATE = "20210503"
         const val DEFAULT_SCHOOL_CD = "7240454"
         const val DEFAULT_CODE = "D10"
+
     }
 }
--- B_Android/presentation/src/main/java/com/simple/b_/base/adapter/BindingAdapter.kt
+++ B_Android/presentation/src/main/java/com/simple/b_/base/adapter/BindingAdapter.kt
@@ -2,7 +2,11 @@ package com.simple.b_.base.adapter
 
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.SnapHelper
+import com.simple.b_.view.adapters.MealAdapter
+import com.simple.data.utils.Constants
 
 class BindingAdapter {
     companion object{
@@ -13,6 +17,11 @@ class BindingAdapter {
             linearLayoutManager.orientation = LinearLayoutManager.HORIZONTAL
             recyclerView?.layoutManager = linearLayoutManager
             recyclerView?.adapter = adapter
+
+            if(adapter is MealAdapter) {
+                val snapHelper = PagerSnapHelper()
+                snapHelper.attachToRecyclerView(recyclerView!!)
+            }
         }
     }
 }
